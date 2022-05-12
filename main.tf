a2e9645cedc94438669012279d9677df6c646c5d
ayildirimnec <87165930+ayildirimnec@users.noreply.github.com>
2022-05-12 16:56:59 +1000

    Update main.tf

--- main.tf
+++ main.tf
@@ -37,7 +37,7 @@ module "enterprise_scale" {
   providers = {
     azurerm              = azurerm
     azurerm.connectivity = azurerm.connectivity
-    azurerm.management   = azurerm
+
   }
   
   root_parent_id = data.azurerm_client_config.core.tenant_id
